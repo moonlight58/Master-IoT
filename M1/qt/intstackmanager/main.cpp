f89f5ed908ce0f6198a8f882c83100fc9cd008f1
moonlight58 <gaelrothlin@gmail.com>
2026-09-14 16:07:36 +0200

    fix(qt): int stack manager exercise

--- M1/qt/intstackmanager/main.cpp
+++ M1/qt/intstackmanager/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    std::srand(static_cast<unsigned>(std::time(nullptr))); // WITHOUT this, "random" is identical every run
+    std::srand(static_cast<unsigned>(std::time(nullptr))); // without this "random" is identical every run, i think
     Widget w;
     w.show();
     return QApplication::exec();
